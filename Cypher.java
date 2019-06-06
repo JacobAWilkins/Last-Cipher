b7432f82566ed426a8b973de2616a145d1cfdd41
Jacob Wilkins <jacobwilkins501@gmail.com>
2019-06-06 01:35:07 -0500

    Update Cypher.java
    add blowfish functionality

--- Cypher.java
+++ Cypher.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Cypher {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		boolean bool = true;
@@ -30,19 +30,29 @@ public class Cypher {
 				}
 			
 				System.out.println("1. A1Z26");
+				System.out.println("2. Blowfish");
 				System.out.print("Enter a Number: ");
 				type = Integer.parseInt(br.readLine());
 				System.out.println();
 			
-				System.out.print("Enter the text to encrypt/decrypt: ");
+				System.out.print("Enter the data to encrypt/decrypt: ");
 				text = br.readLine();
 				System.out.println();
 			
 				switch (type) {
 					case 1:
-						A1Z26 code = new A1Z26();
-						if (proc == 1) { result = code.A1Z26_encrypt(text); }
-						if (proc == 2) { result = code.A1Z26_decrypt(text); }
+						A1Z26 code1 = new A1Z26();
+						if (proc == 1) { result = code1.A1Z26_encrypt(text); }
+						if (proc == 2) { result = code1.A1Z26_decrypt(text); }
+						break;
+					case 2:
+						System.out.print("Enter a secret: ");
+						String pass2 = br.readLine();
+						System.out.println();
+						
+						Blowfish code2 = new Blowfish();
+						if (proc == 1) { result = code2.Blowfish_encrypt(text, pass2); }
+						if (proc == 2) { result = code2.Blowfish_decrypt(text, pass2); }
 						break;
 					
 				}
