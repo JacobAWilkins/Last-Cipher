5f4f1c1536796eadcc5ce87c1b76c9624919bfe1
Jacob Wilkins <jacobwilkins501@gmail.com>
2019-06-11 21:40:19 -0500

    Update base64.java
    fix format 3

--- base64.java
+++ base64.java
@@ -13,18 +13,18 @@
 import java.util.Base64;
 
 public class base64 {
-
-  public static String Base64_encrypt(String text) {
-	  
-	  return Base64.getEncoder().encodeToString(text.getBytes());
-	  
-  }
 	
-  public static String Base64_decrypt(String text) {
-	  
-	  byte[] result = Base64.getDecoder().decode(text);
-	  return new String(result);
-  
-  }
+	public static String Base64_encrypt(String text) {
+		
+		return Base64.getEncoder().encodeToString(text.getBytes());
+	
+	}
+	
+	public static String Base64_decrypt(String text) {
+		
+		byte[] result = Base64.getDecoder().decode(text);
+		return new String(result);
+	
+	}
 
 }
