3486cdf21cf09b9078c10f56ffcf127d8d5a0f29
Jacob Wilkins <jacobwilkins501@gmail.com>
2020-11-30 05:25:08 -0600

    Update Ceaser.java

--- Ceaser.java
+++ Ceaser.java
@@ -18,31 +18,31 @@ public class Ceaser {
 		for (int i = 0; i < text.length(); i++) {
 			char character = text.charAt(i);
 			if (character == 'a') { result = result + "x"; }
-			else if (character == 'b') { result = result + "y"; }
-			else if (character == 'c') { result = result + "z"; }
-			else if (character == 'd') { result = result + "a"; }
-			else if (character == 'e') { result = result + "b"; }
-			else if (character == 'f') { result = result + "c"; }
-			else if (character == 'g') { result = result + "d"; }
-			else if (character == 'h') { result = result + "e"; }
-			else if (character == 'i') { result = result + "f"; }
-			else if (character == 'j') { result = result + "g"; }
-			else if (character == 'k') { result = result + "h"; }
-			else if (character == 'l') { result = result + "i"; }
-			else if (character == 'm') { result = result + "j"; }
-			else if (character == 'n') { result = result + "k"; }
-			else if (character == 'o') { result = result + "l"; }
-			else if (character == 'p') { result = result + "m"; }
-			else if (character == 'q') { result = result + "n"; }
-			else if (character == 'r') { result = result + "o"; }
-			else if (character == 's') { result = result + "p"; }
-			else if (character == 't') { result = result + "q"; }
-			else if (character == 'u') { result = result + "r"; }
-			else if (character == 'v') { result = result + "s"; }
-			else if (character == 'w') { result = result + "t"; }
-			else if (character == 'x') { result = result + "u"; }
-			else if (character == 'y') { result = result + "v"; }
-			else if (character == 'z') { result = result + "w"; }
+			else if (character == 'b') { result += "y"; }
+			else if (character == 'c') { result += "z"; }
+			else if (character == 'd') { result += "a"; }
+			else if (character == 'e') { result += "b"; }
+			else if (character == 'f') { result += "c"; }
+			else if (character == 'g') { result += "d"; }
+			else if (character == 'h') { result += "e"; }
+			else if (character == 'i') { result += "f"; }
+			else if (character == 'j') { result += "g"; }
+			else if (character == 'k') { result += "h"; }
+			else if (character == 'l') { result += "i"; }
+			else if (character == 'm') { result += "j"; }
+			else if (character == 'n') { result += "k"; }
+			else if (character == 'o') { result += "l"; }
+			else if (character == 'p') { result += "m"; }
+			else if (character == 'q') { result += "n"; }
+			else if (character == 'r') { result += "o"; }
+			else if (character == 's') { result += "p"; }
+			else if (character == 't') { result += "q"; }
+			else if (character == 'u') { result += "r"; }
+			else if (character == 'v') { result += "s"; }
+			else if (character == 'w') { result += "t"; }
+			else if (character == 'x') { result += "u"; }
+			else if (character == 'y') { result += "v"; }
+			else if (character == 'z') { result += "w"; }
 			else { result = result + String.valueOf(character); }
 			
 		}
